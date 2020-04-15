e22d7621b3744661412bc1c6f047d92d556e2bd1
Eshi44 <e.hameliervolino@gmail.com>
2020-04-15 17:34:18 -0400

    added controller routes

--- server.js
+++ server.js
@@ -9,21 +9,32 @@ const db = require("./models");
 
 app.use(express.static("public"));
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+
+const pokemonRoutes = require("./controllers/pokemonController.js");
+
+app.use(pokemonRoutes);
+
 app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.get("/api/config", function (req, res) {
+  res.json({
+    success: true,
+  });
+});
 
-db.sequelize.sync({force:true}).then(function () {
+
+db.sequelize.sync({}).then(function () {
   app.listen(PORT, function () {
     console.log(`Server listening on: http://localhost:${PORT}`);
   });
