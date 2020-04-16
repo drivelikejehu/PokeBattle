b4d077086bbd3a2f1802bd31b3b3656744d9818b
John Kang <pollyfactor@hotmail.com>
2020-04-15 21:57:16 -0400

    updates

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 
 const db = require("./models");
 
 app.use(express.static("public"));
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
