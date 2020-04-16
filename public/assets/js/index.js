6e041284aa951dc5acb49bc87795742defc70bbf
Brandon Kim <bkim377@gatech.edu>
2020-04-16 13:20:51 -0400

    Working on route 'localhost:3000/pokemon'

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,8 @@
-$(function () {
-  console.log("Hello World!");
+$(document).ready(function() {
+  // console.log("Hello World!");
+  $("#generate-second-page").on("click", function() {
+    location.assign("/pokemon");
+  })
+
   //event listeners and ajax calls
 });
