d8a40f9db350aca255482ae73ca57fe7add5036a
D3viii <devinsdevs@gmail.com>
2020-04-16 11:45:08 -0400

    SPACES

--- controllers/resultsControllers.js
+++ controllers/resultsControllers.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const db = require("../models");
 
 router.get("/results", function (req, res) {
-    db.User.findAll()
-      .then((users) => {
-        console.log(users);
-        res.render("results", { users });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500);
-        res.json({
-          error: true,
-        });
+  db.User.findAll()
+    .then((users) => {
+      console.log(users);
+      res.render("results", { users });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.json({
+        error: true,
       });
-  });
+    });
+});
 
-  module.exports = router;
+module.exports = router;
