29118d15e33c190ccbb9934e7fff20995bd39946
Eshi44 <e.hameliervolino@gmail.com>
2020-04-17 08:28:42 -0400

    deleted index.js

--- public/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  // console.log("Hello World!");
-  // $("#generate-second-page").on("click", function() {
-  //   location.assign("/pokemon");
-  // })
-
-  //event listeners and ajax calls
-});
