1d8b8c7ec16399d5918fa340aff591fc465293f5
Eshi44 <e.hameliervolino@gmail.com>
2020-04-17 08:38:35 -0400

    added pseudocode to pokemonGenerate js file

--- public/assets/js/pokemonGenerate.js
+++ public/assets/js/pokemonGenerate.js
@@ -1,6 +1,18 @@
+//upon page load- first containers will dynamically generate
+
+
 $(function () {
   console.log("Hello World!");
-  //event listeners and ajax calls
 
-  // var container
+
+// on click function that saves input from user aka username
+//as well as randomly selects pokemon from ajax call math.random and
+//saves pokemon along with type and username in mysql
+
+//this ^^ will be done twice & seperatly. One for user 1 and one for user 2
+//conditional that checks that both users have generated a pokemon
+
+
+
+
 });
