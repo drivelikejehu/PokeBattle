30e8701b1011c0b439acc8332e9b57534ff68d70
Eshi44 <e.hameliervolino@gmail.com>
2020-04-17 15:38:41 -0400

    fixed on click function

--- controllers/pokemonGenerateController.js
+++ controllers/pokemonGenerateController.js
@@ -32,37 +32,51 @@ router.get("/pokemon", function (req, res) {
 //     });
 // });
 
-router.get("/api/pokemon", function (req, res) {
-  db.Pokemon.findAll()
-    .then((pokemons) => {
-      console.log(pokemons);
-      res.json(pokemons);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500);
-      res.json({
-        error: true,
-      });
-    });
+// router.get("/api/pokemon", function (req, res) {
+//   db.Pokemon.findAll()
+//     .then((pokemons) => {
+//       console.log(pokemons);
+//       res.json(pokemons);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500);
+//       res.json({
+//         error: true,
+//       });
+//     });
+// });
+
+
+// eslint-disable-next-line no-unused-vars
+router.post("/api/pokemon", function(req, res) {
+  db.Pokemon.insertOne([
+    "userName"
+  ], [
+    req.body.userName
+  ], function(res) {
+    console.log(res);
+  });
 });
 
-router.get("/api/pokemon/:id", function (req, res) {
-  db.Lego.findOne({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((pokemons) => {
-      console.log(pokemons);
-      res.json(pokemons);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500);
-      res.json({
-        error: true,
-      });
-    });
-});
+
+
+// router.get("/api/pokemon/:id", function (req, res) {
+//   db.Lego.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((pokemons) => {
+//       console.log(pokemons);
+//       res.json(pokemons);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500);
+//       res.json({
+//         error: true,
+//       });
+//     });
+// });
 module.exports = router;
--- public/assets/js/pokemonGenerate.js
+++ public/assets/js/pokemonGenerate.js
@@ -4,21 +4,52 @@ $(document).ready(function() {
 
   function getCardSectionsToGeneratePokemon() {
     const generateCardSectionsOne = $(`<h4>Username</h4>
-  <input type="text">
-  <a href="/pokemon"><img alt="pokemon ball" src="/assets/img/pokemon-ball.png" id="generate-second-page"></img></a>`);
-
+    <div class="form">
+  <input id="inputUsernameOne" type="text" name="pokeUserName"> </div>
+  <img alt="pokemon ball" type="button" src="/assets/img/pokemon-ball.png" id="pokemonBallOne"></img>
+  `);
     const generateCardSectionsTwo = $(`<h4>Username</h4>
-  <input type="text">
-  <a href="/pokemon"><img alt="pokemon ball" src="/assets/img/pokemon-ball.png" id="generate-second-page"></img></a>`);
+  <input id="inputUsernameTwo" type="text">
+  <a href="/pokemon"><img alt="pokemon ball" type="submit" type="button" src="/assets/img/pokemon-ball.png" id="pokemonBallTwo"></img></a>`);
       //append to card-sections
     $("#card-user-One").append(generateCardSectionsOne);
     $("#card-user-Two").append(generateCardSectionsTwo);
 
   }
   getCardSectionsToGeneratePokemon();
+
+
+  // on click function that saves input from user aka username
+  console.log($("#pokemonBallOne"));
+  $("#pokemonBallOne").on("click",function() {
+    console.log("hi");
+    var pokeBallOne = {
+      userName: $("#inputUsernameOne").val()
+    };
+    console.log(pokeBallOne);
+    $.ajax("api/pokemon", {
+      type: "POST",
+      data: pokeBallOne
+    }).then(
+      function(){
+        console.log("done!");
+      }
+    );
+  });
+
+
+
 });
 
-// on click function that saves input from user aka username
+
+
+
+
+
+
+
+
+
 //as well as randomly selects pokemon from ajax call math.random and
 //saves pokemon along with type and username in mysql
 
@@ -33,6 +64,3 @@ $(document).ready(function() {
 //what pokemon type beats what). *if type=type, then math.random to decide
 //user will be direced to results page which will display that user/pokemon that won
 //info will be saved to mysql(done in resultsController file)
-$(function () {
-  console.log("Hello World!");
-});
