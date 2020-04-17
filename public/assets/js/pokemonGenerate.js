af7709adec166bb4e14ce2f07cedb83d86900930
Eshi44 <e.hameliervolino@gmail.com>
2020-04-17 08:45:25 -0400

    added pseudocode for pokemonGenerate

--- public/assets/js/pokemonGenerate.js
+++ public/assets/js/pokemonGenerate.js
@@ -10,7 +10,17 @@ $(function () {
 //saves pokemon along with type and username in mysql
 
 //this ^^ will be done twice & seperatly. One for user 1 and one for user 2
-//conditional that checks that both users have generated a pokemon
+//upon the click that generates and saves user/pokemon info, card will empty
+//and a 'You got ${pokemon}!' with type and picture of pokemon will be dynamically created
+
+//write conditional that checks both users have generated a pokemon
+//if both users have generated a pokemon/info saved,
+//then a submit button will be dynammically generated
+//upon submit- a query will run to compare types of pokemon(need to write conditionals as to
+//what pokemon type beats what). *if type=type, then math.random to decide
+//user will be direced to results page which will display that user/pokemon that won
+//info will be saved to mysql(done in resultsController file)
+
 
 
 
