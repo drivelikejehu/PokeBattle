d68785de9a41c5f606abf86160db2e2347f04827
John Kang <pollyfactor@hotmail.com>
2020-04-18 14:57:35 -0400

    changed handlebars

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     userName: DataTypes.STRING,
     pokemonName: DataTypes.STRING,
-    // imageUrl: DataTypes.STRING,
+    // wins: DataTypes.INTEGER,
+    // losses: DataTypes.INTEGER,
   });
   User.associate = function(models) {
     User.hasMany(models.Pokemon, {
