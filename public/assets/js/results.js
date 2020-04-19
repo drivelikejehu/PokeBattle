a1da31c99c5dcaf0d4bf1cb169b2b2f8b30d9640
D3viii <devinsdevs@gmail.com>
2020-04-19 15:15:03 -0400

    Results

--- public/assets/js/results.js
+++ public/assets/js/results.js
@@ -1,47 +1,50 @@
- // event listeners and ajax calls
-  // Upon clicking the pokeball on the pokemonGenerate page, this page will dynamically generate the winner of the battle
-  // Must compare the XP parameter of the two Pokemon and see which one is higher - that Pokemon will win
-  // obtain users' win and loss data from MySQL database and increase their win or loss count by 1 each
-  // GET route to pokemon_db.Users
-  var Pokemon = require("../models/pokemon.js");
-  var User = require("../models/user.js");
+// event listeners and ajax calls
+// Upon clicking the pokeball on the pokemonGenerate page, this page will dynamically generate the winner of the battle
+// Must compare the XP parameter of the two Pokemon and see which one is higher - that Pokemon will win
+// obtain users' win and loss data from MySQL database and increase their win or loss count by 1 each
+// GET route to pokemon_db.Users
+var Pokemon = require("../models/pokemon.js");
+var User = require("../models/user.js");
 
-  $(document).ready(function() {
+$(document).ready(function () {
+  module.exports = function (app) {
+    app.get("/pokemon", function (req, res) {
+      // comparePokemonXP();
+      // set variables to get both pokemon's XP values
+      var pokemonOneXP = $("#userOne").pokeBall.xp;
+      var pokemonTwoXP = $("#userTwo").pokeBall.xp;
 
- 
-  module.exports = function(app){
- 
-  app.get("/pokemon", function (req, res) {
-    // comparePokemonXP();
-    // set variables to get both pokemon's XP values
-  var pokemonOneXP = $("#userOne").pokeBall.xp;
-  var pokemonTwoXP = $("#userTwo").pokeBall.xp;
+      if (pokemonOneXP > pokemonTwoXP) {
+        Pokemon.findOne({
+          where: {
+            routeName: req.params.characters,
+          },
+        }).then(function (result) {
+          return res.json(result);
+        });
+      } else if (pokemonTwoXP > pokemonOneXP) {
+        Pokemon.findAll().then(function (result) {
+          return res.json(result);
+        });
+      }
+    });
 
-    if (pokemonOneXP > pokemonTwoXP) {
-      Pokemon.findOne({
-        where: {
-          routeName: req.params.characters
-        }
-      }).then(function(result) {
-        return res.json(result);
-      });
-    } else if (pokemonTwoXP > pokemonOneXP) {
-      Pokemon.findAll().then(function(result) {
-        return res.json(result);
-      });
+    function comparePokemonXP() {
+      // set conditionals to compare XP values
+      if (pokemonOneXP > pokemonTwoXP) {
+        $("#winnerName").append("testUser1");
+      } else if (pokemonTwoXP > pokemonOneXP) {
+        $("#winnerName").append("testUser2");
+      }
     }
-  });
 
-   function comparePokemonXP(){
-    // set conditionals to compare XP values
-    if (pokemonOneXP > pokemonTwoXP) {
-      $("#winnerName").append("testUser1");
-    } else if (pokemonTwoXP > pokemonOneXP) {
-      $("#winnerName").append("testUser2");
-    }
-   }
+    comparePokemonXP()
 
-  app.post("/pokemon", function (req, res) {
+    console.log(comparePokemonXP);
+
+    // app.post("/pokemon", function (req, res) {
+    //   console.log(comparePokemonXP)
+
     // // Take the request...
     // var character = req.body;
     // // Create a routeName
@@ -57,6 +60,5 @@
     //   forcePoints: character.forcePoints
     // });
     // res.status(204).end();
-  });
   };
 });
